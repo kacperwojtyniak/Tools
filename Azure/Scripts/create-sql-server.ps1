c9a502d69a43f6f76205892dc462d8075dc45122
Kacper Wojtyniak <38474178+kacperwojtyniak@users.noreply.github.com>
2022-04-03 11:46:08 +0200

    Add script to create sql server

---
+++ Azure/Scripts/create-sql-server.ps1
@@ -0,0 +1,8 @@
+$rg = 'rg-kw-003'
+$l='swedencentral'
+$server = 'sql-kw-001'
+
+az sql server create -n $server --location $l -g $rg -u kacper -p 'abcABC123'
+
+# Allow access from azure services
+az sql server firewall-rule create -g $rg -s $server -n azureservices --start-ip-address 0.0.0.0 --end-ip-address 0.0.0.0
