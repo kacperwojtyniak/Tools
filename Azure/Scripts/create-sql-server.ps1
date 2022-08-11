fbf3ae8d34f02507a57546b81a317dec231f659a
kacperwojtyniak <wojtyniak.kacper@gmail.com>
2022-08-11 17:32:48 +0200

    SQL TLS version

--- Azure/Scripts/create-sql-server.ps1
+++ Azure/Scripts/create-sql-server.ps1
@@ -2,7 +2,7 @@ $rg = 'rg-kw-003'
 $l='swedencentral'
 $server = 'sql-kw-001'
 
-az sql server create -n $server --location $l -g $rg -u kacper -p 'abcABC123'
+az sql server create -n $server --location $l -g $rg -u kacper -p 'abcABC123' --minimal-tls-version 1.2
 
 # Allow access from azure services
 az sql server firewall-rule create -g $rg -s $server -n azureservices --start-ip-address 0.0.0.0 --end-ip-address 0.0.0.0
